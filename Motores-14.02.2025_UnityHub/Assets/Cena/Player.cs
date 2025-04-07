9594ab2144574dd7c5d8395f4dfae890488ab8bf
MariaJulia1207 <maria.palhares@escolar.ifrn.edu.br>
2025-04-07 11:04:33 -0300

    07/04/2025 - 11:05
    Maria Júlia Palhares
    PJD2M
    Fundamentos de Inteligência Artificial para Jogos Digitais II

--- Motores-14.02.2025_UnityHub/Assets/Cena/Player.cs
+++ Motores-14.02.2025_UnityHub/Assets/Cena/Player.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  public float velocidade = 10f;
-  public float focaPulo = 10f;
+    public float velocidade = 10f;
+    public float focaPulo = 10f;
 
     public bool noChao = false;
 
     public bool andando = false;
 
-  private Rigidbody2D _rigidbody2D;
-  private SpriteRenderer  _spriteRenderer;
-  private Animator _animator;
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +24,7 @@ public class Player : MonoBehaviour
     }
 
 
-   void OnCollisionStay2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "chao")
         {
@@ -44,43 +44,43 @@ public class Player : MonoBehaviour
     void Update()
     {
         andando = false;
-        
-      if(Input.GetKey(KeyCode.LeftArrow))
-      {
-        gameObject.transform.position += new Vector3(-velocidade*Time.deltaTime,0,0);
-        //rigidbody2D.AddForce(new Vector2(-velocidade,0));
-        _spriteRenderer.flipX = true;
-        Debug.Log("LeftArrow");
 
-        if (noChao == true)
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            andando = true;
-        }
-      }
-        
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+            //rigidbody2D.AddForce(new Vector2(-velocidade,0));
+            _spriteRenderer.flipX = true;
+            Debug.Log("LeftArrow");
 
-      if(Input.GetKey(KeyCode.RightArrow))
-      {
-        gameObject.transform.position += new Vector3(velocidade*Time.deltaTime,0,0);
-        //rigidbody2D.AddForce(new Vector2(velocidade,0));
-         _spriteRenderer.flipX = false;
-         Debug.Log("RightArrow");
-         
-         if (noChao == true)
-         {
-             andando = true;
-         }
-      }
+            if (noChao == true)
+            {
+                andando = true;
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+            //rigidbody2D.AddForce(new Vector2(velocidade,0));
+            _spriteRenderer.flipX = false;
+            Debug.Log("RightArrow");
+
+            if (noChao == true)
+            {
+                andando = true;
+            }
+        }
 
         if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
         {
-            _rigidbody2D.AddForce(new Vector2(0, 1) * focaPulo,ForceMode2D.Impulse);
+            _rigidbody2D.AddForce(new Vector2(0, 1) * focaPulo, ForceMode2D.Impulse);
 
             Debug.Log("Jump");
         }
 
-        _animator.SetBool("Andando",andando);
-        
-     
+        _animator.SetBool("Andando", andando);
+
+
     }
 }
